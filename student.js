f04a6f0e6415d74a47a09f74fb784905f080e192
skilsafari <143485777+skilsafari@users.noreply.github.com>
2023-11-30 15:00:20 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,71 @@
+//object
+//let name = "John";
+ 
+let person = {
+  //key:value
+  name : "John",
+  Contact : 9578303661,
+  email : "deepapolice@gmail.com",
+
+};
+console.log(person);
+
+//let book= " A Life in Movies";
+
+
+let book = {
+  //key:value
+   name : " A Life in Movies",
+   author : " Gupta",
+   price : 450,
+   bookstore : "CLEVER BOOKSTORE NAME IDEAS",
+};
+console.log(book)
+ 
+ function celsiusToFahrenheit(celsius) {
+  const fahrenheit = celsius * 9/5 + 32;
+  return fahrenheit;
+};
+
+console.log(celsiusToFahrenheit(25));
+
+//let employee = "John";
+let employee = {
+  //key:value
+  name : "John",
+  position : "supervisor",
+  salary : 20000,
+  experience : "12 year",
+  age : 39,
+};
+console.log(employee)
+//let item = "food",
+let item = {
+  //key:value
+  item : "food",
+  name : "rice",
+  quality : "good",
+  price : 80,
+  items :"meat, juice, ice-cream"
+};
+console.log(item)
+//let student = "John",
+let student = {
+  //key:value
+  name : "John",
+  age : 15,
+  gender : "male",
+  contact : 9788096826,
+  grade : "A",
+  character : "very good",
+};
+console.log(student)
+//let library = "book",
+let library = { 
+  //key:value
+  checkout : "life of history, silent, success of life, ",
+return : "History of india, live in india, friendship in the world",
+overdue : "food, failure is not life, silent is better life, medicine, ",
+};
+console.log(library)
+//object
